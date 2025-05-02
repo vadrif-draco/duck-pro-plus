0971678ef153660db6662faf02fbc017aebd825f
Ahmad S. Salama <vadrif.draco@gmail.com>
2025-05-02 15:46:02 +0300

    waterbase

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable, of, throwError } from "rxjs";
 import { catchError, map, tap } from "rxjs/operators";
-import { environment } from "../../../environments/environment";
+import { environment } from "../../environments/environment";
 
 export interface DuckData {
   id: string;
